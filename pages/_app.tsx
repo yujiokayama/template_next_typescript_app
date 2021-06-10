4ac55e93cc42484aa2f297cfca2b16072fc7eea7
Yuji Okayama <okayama-yu@gnavi.co.jp>
2021-06-11 08:56:06 +0900

    fix page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <RecoilRoot>
+        <Component {...pageProps} />;
+      </RecoilRoot>
+    </div>
+  );
 };
 
 export default MyApp;
